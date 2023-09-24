8d5ba2f9729c808362bec75ce6f22394bb3946ee
UniCornPrincess69 <115454083+UniCornPrincess69@users.noreply.github.com>
2023-09-24 12:45:49 +0200

    Creation of the project
    Creation of the first headers and cpp

---
+++ Snake/ErrorType.h
@@ -0,0 +1,10 @@
+#ifndef ERROR_TYPE_H
+#define ERROR_TYPE_H
+
+enum class ErrorType : char
+{
+	ET_SUCCESS = 0,
+	ET_COUNT
+};
+
+#endif // !ERROR_TYPE_H
---
+++ Snake/InputHandler.h
@@ -0,0 +1,8 @@
+#ifndef INPUT_HANDLER_H
+#define INPUT_HANDLER_H
+
+class CInputHandler
+{
+};
+
+#endif // !INPUT_HANDLER_H
